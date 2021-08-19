400a849e6f1b31edcaccc9bdc58704e23e61c077
wangyunfei <yfwang@cygnusbio.com>
2021-08-19 22:19:37 +0800

    第一课 打印hello world

---
+++ code_1/demo.c
@@ -0,0 +1,16 @@
+// 引用stdio.h文件
+// stdio.h   std： std：standard i:input o:output 标准输入输出文件
+#include <stdio.h>
+
+// main主函数-程序入口，一个文件只有一个main函数
+// int代表整型  main函数前添加一个int代表main返回个正向
+int main(){
+    // 这里为要运行的任务
+    // 在屏幕上输出 hello world
+    // 使用print function --printf 打印函数
+    // printf为C语言提供的函数，所以需要引用stdio.h文件
+    printf("hehe world!\n");
+    return 0;
+}
+
+
